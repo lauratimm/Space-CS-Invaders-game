19d65178ba90d437d54d279939e1d7b8c2b50545
lauratimm <47371433+lauratimm@users.noreply.github.com>
2019-02-19 18:22:58 -0700

    Update Enemies.java

--- Enemies.java
+++ Enemies.java
@@ -1,12 +1,16 @@
-public class Enemies {
+public class Enemies extends Space_Board {
   char alien = 'Z'
 
-public void generateEnemies(){
+//use randam to choose where the enemies will be placed on the board
+public void generateEnemies(char alien, int row, int column){
+  grid[row][column] = alien;
 
 }
+// use the bullet location to remove the enemy that is hit there
 public void removeEnemies(){
 
 }
+// after each turn move the enemy based on a boolean
 public void moveEnemies(){
 
 }
