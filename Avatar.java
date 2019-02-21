f171e244020ee54cef3824a58a79746d482034c0
abbydelisle <47436661+abbydelisle@users.noreply.github.com>
2019-02-20 22:26:56 -0700

    Add files via upload
    Skeleton for avatar class

---
+++ Avatar.java
@@ -0,0 +1,20 @@
+public class Avatar extends Space_Board {
+
+  private char ship = 'S';
+
+  // I'm not sure how we're gonna go about getting the location of the things
+  // on the screen, this is just the list indices as "x and y" coordinates.
+  // Open to suggestions!!!
+  private int row = 9;
+  private int column = 5;
+
+  // Based on user input (w,a,s or d) move one "space" in a specific direction
+  // Will also need to remove the ship from the previous space.
+  public void moveShip(input) {
+  }
+
+  // The bullet will need to know where the ship is so it can shoot from
+  // the right place.
+  public int getLocation() {
+  }
+}
