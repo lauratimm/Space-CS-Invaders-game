a2237de43123ab6a6e9c61481761c81e5343810c
jhc3323 <47798526+jhc3323@users.noreply.github.com>
2019-03-19 15:09:49 -0600

    Add files via upload

---
+++ BossFight.java
@@ -0,0 +1,179 @@
+import javafx.application.Application;
+import java.awt.*;
+import java.awt.Label;
+import java.awt.event.KeyEvent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+//import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.geometry.*;
+import javafx.animation.AnimationTimer;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.input.KeyCode;
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.*;
+import java.util.ArrayList;
+import javafx.scene.control.Button;
+import javafx.application.Platform;
+import java.io.*;
+
+public class BossFight extends Spaces{
+
+  Stage window;
+	Scene scene;
+	boolean avatar_dead = false;
+  boolean boss_dead = false;
+	double t = 0;
+  Pane layout = new Pane();
+  Image image = new Image("avatar.png");
+  Image image3 = new Image("heart.png");
+  Heart heart = new Heart();
+  Avatar avatar = new Avatar(300, 500, 60, 60, "avatar", image);
+  Image imageBoss = new Image("boss.png");
+  ArrayList<Character> numli = new ArrayList<Character>(5);
+  //ArrayList<String> bossHp = new ArrayList<String>(50);
+  //String joined = String.join("",bossHp);
+  Boss boss = new Boss(150,70, 300, 200, "boss", imageBoss);
+  //Text t = new Text(200,400,joined);
+
+
+  public static void main(String[] args){
+    launch(args);
+  }
+  @Override
+	public void start(Stage primaryStage) throws Exception{
+    /*
+    btn.setOnAction(e -> {
+      if(menu("..", "exit?")){
+        System.exit(0);
+      }
+    });
+    */
+		window = primaryStage;
+		window.setTitle("Boss fight");
+    //https://stackoverflow.com/questions/22841000/how-to-change-the-color-of-pane-in-javafx
+    layout.setStyle("-fx-background-color:black;");
+		layout.getChildren().add(avatar);
+    //btn.relocate(560,8);
+    //layout.getChildren().add(btn);
+
+		layout.getChildren().addAll(boss);
+
+		/*
+    layout2.setStyle("-fx-background-color:black;");
+    layout2.getChildren().addAll(btn);
+    btn.setOnAction(ev -> {
+      menu("Game Over", "exit?");
+    });
+    //layout2.setAlignment(Pos.CENTER);
+		*/
+
+		AnimationTimer bossTimer = new AnimationTimer(){
+
+
+	@Override
+	public void handle(long now){
+
+		//alien.setEnemyList(alien, alien2, alien3, alien4, alien5);
+		//enemyList = alien.getEnemyList();
+		if (avatar_dead) {
+			stop();
+			//quit = 2;
+			MenuBox.display("Menu", "You Lost!", "lose");
+			//menu("Menu","You lost!");
+
+
+		}
+		if (boss_dead) {
+			stop();
+			//quit = 1;
+			MenuBox.display("Menu", "You Won!", "win");
+
+		}
+
+
+    //shoot(boss, Color.PINK);
+
+
+      }
+    };
+
+    	bossTimer.start();
+
+    	scene = new Scene(layout, 600, 800);
+      //scene2 = new Scene(layout2, 300, 400);
+    	scene.setOnKeyPressed(e -> {
+
+				if (e.getCode() == KeyCode.Q) {
+    		//quit = 0;
+				MenuBox.display("Menu", "Game Menu", "Q");
+
+    	}
+
+    		else if (avatar.movement(e.getCode()) == true){
+    			shoot(avatar,Color.YELLOW);
+    		}
+
+    });
+
+
+    heart.numHeart(numli, avatar, image3);
+    //Label barcode = new Label("dfsf");
+    //barcode.relocate(100,760);
+    layout.getChildren().addAll(numli);
+    boss.numHp();
+    //System.out.println(boss.shp(bossHp));
+    System.out.println(boss.shp());
+    //boss.thp();
+
+    boss.setHp();
+    layout.getChildren().add(boss.t);
+
+    window.setScene(scene);
+    window.show();
+	}
+
+  public void shoot(Character p, Color c){
+    Bullet bullet = new Bullet((int) p.getX() + 20, (int) p.getY(), 5, 20, p.type+"Bullet", c);
+    layout.getChildren().add(bullet);
+
+    AnimationTimer bulletTimer = new AnimationTimer(){
+
+		@Override
+		public void handle(long now){
+			int idk = bullet.bossShooter(avatar, boss, layout, heart, numli, image3);
+
+			if(idk == 2) {
+				stop();
+			}
+
+			if (avatar.getLife() == 0) {
+				avatar.delete();
+				stop();
+				avatar_dead = true;
+
+			}
+			if (boss.getBLife() == 0) {
+				boss_dead = true;
+			}
+
+
+		}
+    };
+    bulletTimer.start();
+
+
+  }
+
+}
--- Bullet.java
+++ Bullet.java
@@ -1,107 +1,153 @@
 import java.util.ArrayList;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.layout.*;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.geometry.*;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
-//This class creates a Bullet object. The class extends Rectangle. 
-//When a Bullet is created, the width, height and color are used by the Rectangle 
-//parent class and and x and y coordinates of the object, and a String are assigned
-//to the Bullet object. The class has methods to move the bullet up, down 
-//and for when Avatar shoots
 public class Bullet extends Rectangle{
-	
-	// Instance Variables
-	private String type; // indicates the name of the object
-	
-	// The constructor creates a Bullet with width, height, and color by 
-	// calling rectangle and assigns an x and y coordinate to the rectangle 
-	// with a String type
+
+	private String type;
+
+	// Bullet
 	public Bullet(int x, int y, int w, int h, String t, Color color){
 		super(w, h, color);
 		this.type = t;
 		setTranslateX(x);
 		setTranslateY(y);
-		}
+	}
 
-	// Setter method for the type
-	public void setType(String type) {
-		this.type = type;
-		}
-	
-	// Getter method for the type
-	public String getType() {
-		return type;
-		}
-	
-	// Moves the Bullet up
 	public void moveUp(){
 		setTranslateY(getTranslateY() - 5);
-		}
-	// Move the Bullet Down
+	}
+
 	public void moveDown(){
 		setTranslateY(getTranslateY() + 3);
-		}
-	
-	// This method is called when the Avatar or Enemy shoots, and uses the Avatar,
-	// the Pane, Heart, list of Hearts, and the image of hearts to move the
-	// Bullet up, detect collision with an enemy or with Avatar, to revmoe the Enemy
-	// or lose a heart. And returns whether the Avatar was hit or not
-	public boolean shooter (Avatar avatar, ArrayList<Enemy> enemyList, Pane layout, Heart heart, 
-			ArrayList<Character>numli, Image image3) {
-		
-		boolean avatar_hit = false; // boolean for if avatar is hit
-		
-		// for every single enemy
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getType() {
+		return type;
+	}
+	public int shooter (Avatar avatar, ArrayList<Enemy> enemyList, Pane layout, Heart heart, ArrayList<Character>numli, Image image3) {
+		int ret = 0;
 		for (int i = 0; i < 5; i++) {
-			
-			// if the type of bullet is from an Avatar
 			if (this.getType().equals("avatarBullet")){
-				// if the bullet does not intersect with an enemy, move the bullet up
-				// https://www.youtube.com/watch?v=FVo1fm52hz0&feature=youtu.be
-				if (!this.getBoundsInParent().intersects(enemyList.get(i).getBoundsInParent()) 
-						&& this.getTranslateY() > -20) {
+				if (!this.getBoundsInParent().intersects(enemyList.get(i).getBoundsInParent()) && this.getTranslateY() > -20) {
 					this.moveUp();
-					}
-				// if the bullet does intersect with an enemy, set enemy to dead, remove the bullet and enemy
-				// from the Pane, delete enemy from the list, and add one to number of enemies avatar has killed
+				}
 				else if (this.getBoundsInParent().intersects(enemyList.get(i).getBoundsInParent()) ){
 					enemyList.get(i).dead = true;
 					layout.getChildren().remove(enemyList.get(i));
 					layout.getChildren().remove(this);
 					enemyList.get(i).delete();
 					avatar.setE_killed(avatar.getE_killed() + 1);
-					}
-				}
-			
-			// if the type of bullet is from an Enemy
-			else if (this.getType().equals("enemyBullet") ){
-				// if the bullet does not intersect with the Avatar, move the bullet down
-				if (!this.getBoundsInParent().intersects(avatar.getBoundsInParent()) 
-						&& this.getTranslateY() < 800){
-					this.moveDown();
-					}
-				// if the bullet does intersect with the Avatar, remove the bullet, 
-				// and set avatar to true
-				else if (this.getBoundsInParent().intersects(avatar.getBoundsInParent())){
-					layout.getChildren().remove(this);
-					avatar_hit = true;
-					}
 				}
 			}
-		
-		// if the bullet does intersect with the Avatar, and the life is greater than
-		// 0, then decrease the life, remove the hearts then add the new number of hearts
-		// This is outside the for loop as it is not supposed to loop for every single enemy
-		// Only for one bullet
+			else if (this.getType().equals("enemyBullet") ){
+				int count = 0;
+				if (!this.getBoundsInParent().intersects(avatar.getBoundsInParent()) && this.getTranslateY() < 800){
+					this.moveDown();
+				}
+				else if (this.getBoundsInParent().intersects(avatar.getBoundsInParent())){
+					layout.getChildren().remove(this);
+					//layout.getChildren().remove(avatar);
+					ret = 2;
+				}
+			}
+
+		}
 		if (this.getBoundsInParent().intersects(avatar.getBoundsInParent())){
 			if (avatar.getLife()>0){
 				avatar.loseLife();
 				layout.getChildren().removeAll(numli);
 				heart.removeHeart(numli, avatar, image3);
 				layout.getChildren().addAll(numli);
-				}
+				System.out. println(avatar.getLife());
 			}
-		return avatar_hit;
+
+	}
+
+
+
+		return ret;
+	}
+
+
+
+	public int bossShooter (Avatar avatar, Boss boss, Pane layout, Heart heart, ArrayList<Character>numli, Image image3) {
+		int ret = 0;
+		if (this.getType().equals("avatarBullet")){
+			if (!this.getBoundsInParent().intersects(boss.getBoundsInParent()) && this.getTranslateY() > -20) {
+				this.moveUp();
+			}
+			else if (this.getBoundsInParent().intersects(boss.getBoundsInParent()) ) {
+				if (boss.getBLife()>0){
+					layout.getChildren().remove(this);
+					//Text te = boss.thp();
+					//layout.getChildren().removeAll(te);
+					layout.getChildren().remove(boss.t);
+					boss.loseBLife();
+					boss.removeHp();
+					boss.setHp();
+					layout.getChildren().add(boss.t);
+					//Text te = boss.thp();
+
+					//layout.getChildren().add(boss.thp());
+
+					//boss.rthp(layout, t);
+					//Text te = boss.thp();
+					//layout.getChildren().add(te)	;
+					System.out.println(boss.shp());
+
+					ret = 2;
+				}
+
+				//boss.loseBLife();
+				//this = null;
+			}
 		}
+		/*
+		else if (this.getType().equals("bossBullet") ){
+			int count = 0;
+			if (!this.getBoundsInParent().intersects(avatar.getBoundsInParent()) && this.getTranslateY() < 800){
+				this.moveDown();
+			}
+			else if (this.getBoundsInParent().intersects(avatar.getBoundsInParent())){
+				layout.getChildren().remove(this);
+				//layout.getChildren().remove(avatar);
+				ret = 2;
+			}
+		}
+		*/
+		/*
+		if (this.getBoundsInParent().intersects(avatar.getBoundsInParent())){
+			if (avatar.getLife()>0){
+				avatar.loseLife();
+				layout.getChildren().removeAll(numli);
+				heart.removeHeart(numli, avatar, image3);
+				layout.getChildren().addAll(numli);
+				System.out. println(avatar.getLife());
+			}
+
+
+		}
+		*/
+
+		return ret;
+	}
+
+
+
 }
