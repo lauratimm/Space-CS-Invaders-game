c85afa746e05c310bb7ede40f239b92c02a2e4df
abbydelisle <47436661+abbydelisle@users.noreply.github.com>
2019-03-24 15:19:16 -0600

    Update Character.java

--- Character.java
+++ Character.java
@@ -12,6 +12,11 @@ public class Character extends ImageView {
 	public boolean dead; // indicates whether the object is dead or not
 	public String type; // indicates the name of the object
 
+	static final int deleteWidth = 1;
+	static final int deleteHeight = 1;
+	static final int deleteX = -100;
+	static final int deleteY = -100;
+
 	// Constructor which assigns an image to the object, along with the width,
 	// height, and x and y coordinates of the object, and String type
 	public Character(int xCoord, int yCoord, int width, int height, String type, Image image){
@@ -27,9 +32,9 @@ public class Character extends ImageView {
 	// Method which 'deletes' the object from the main stage by moving the object
 	// outside the viewable stage
 	public void delete() {
-		setFitWidth(1);
-		setFitHeight(1);
-		setX(-100);
-		setY(-100);
+		setFitWidth(deleteWidth);
+		setFitHeight(deleteHeight);
+		setX(deleteX);
+		setY(deleteY);
 		}
 }
